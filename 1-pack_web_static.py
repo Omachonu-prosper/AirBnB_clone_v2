799154cec26e1c90064aa3d239311bf5d4436af4
Omachonu Prosper <omachonucodes@gmail.com>
2023-09-16 07:33:27 +0000

    Validate pep8 code style

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,14 +9,22 @@ from datetime import datetime
 
 
 def do_pack():
-	"""
-	Pack contents of the web_static directory into .tgz archive
-	"""
-	try:
-		current_time = datetime.now()
-		time_format = current_time.strftime('%Y%m%d%H%M%S')
-		print("Packing web_static to versions/web_static_{}.tgz".format(time_format))
-		local("tar -cvzf versions/web_static_{}.tgz web_static".format(time_format))
-		return "versions/web_static_{}.tgz".format(time_format)
-	except:
-		return None
+    """
+    Pack contents of the web_static directory into .tgz archive
+    """
+    try:
+        current_time = datetime.now()
+        time_format = current_time.strftime('%Y%m%d%H%M%S')
+        print(
+            "Packing web_static to versions/web_static_{}.tgz".format(
+                time_format
+            )
+        )
+        local(
+            "tar -cvzf versions/web_static_{}.tgz web_static".format(
+                time_format
+            )
+        )
+        return "versions/web_static_{}.tgz".format(time_format)
+    except:
+        return None
